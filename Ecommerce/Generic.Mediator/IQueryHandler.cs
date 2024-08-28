2159875acd4c936ae9910de0eba1b3d030ca302e
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-08-28 15:24:28 +0200

    Have added some changes

--- Ecommerce/Generic.Mediator/IQueryHandler.cs
+++ Ecommerce/Generic.Mediator/IQueryHandler.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
 
 namespace Generic.Mediator
 {
-    internal interface IQueryHandler
+    public interface IQueryHandler<in TQuery, TRes> : IRequestHandler<TQuery, TRes> where TQuery : IQuery<TRes>
     {
     }
 }
